565e502a98d6cd0cb96072a50c075f5ed095edd7
Manishkumarsingh41 <91113108+Manishkumarsingh41@users.noreply.github.com>
2022-08-06 15:19:35 +0530

    Create AddTwoNumber.c
    Added code for summation of two numbers

---
+++ AddTwoNumber.c
@@ -0,0 +1,12 @@
+#include <stdio.h>
+
+int main(){
+    int x,y;
+    int sum = 0;
+    printf ("Enter x & y number\n");
+    scanf("%d%d",&x,&y);
+    sum=x+y;
+    printf("sum of x & y = %d",sum);
+    
+    return 0;
+}
