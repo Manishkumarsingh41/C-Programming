24f4342c08d922511fbc0e2d20381581fc82b600
Manish Kumar Singh <91113108+Manishkumarsingh41@users.noreply.github.com>
2022-09-21 19:39:34 +0530

    Add files via upload
    C program for step by step evaluation

---
+++ step_by_step_evaluation.c
@@ -0,0 +1,12 @@
+#include<stdio.h>
+
+int main (){
+    int x = 2, y=3, z=3, k=1;
+    int result = 3 * x / y - z + k;
+    // 6/3 - 3+1
+    // 2 - 3+1
+    // 3 - 3
+    // 0
+    printf("The value of result is %d", result);
+    return 0;
+}
