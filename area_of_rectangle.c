5562539c9da906ab05ea88bc06a99cad5d7e8a8a
Manish Kumar Singh <91113108+Manishkumarsingh41@users.noreply.github.com>
2022-09-23 18:40:17 +0530

    Add files via upload
    C program to find area of rectangle

---
+++ area_of_rectangle.c
@@ -0,0 +1,15 @@
+#include <stdio.h>
+
+
+int main()
+{
+    int length, breadth;
+    printf("What is the length of your rectangle\n");
+    scanf("%d", &length);
+
+    printf("What is the breadth of your rectangle\n");
+    scanf("%d", &breadth);
+
+    printf("The area of your ractangle is %d", length*breadth);
+    return 0;
+}
