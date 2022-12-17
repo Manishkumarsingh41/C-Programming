bfa4d594f93140379839f87efd25bf304ffda6fc
Manish Kumar Singh <91113108+Manishkumarsingh41@users.noreply.github.com>
2022-12-17 13:07:15 +0530

    Add files via upload
    Program to illustrate relational operators.

---
+++ illustrate_rational_operators.c
@@ -0,0 +1,25 @@
+// Program to illustrate relational operators.
+
+#include<stdio.h>
+
+void main ()
+{   int R,a,b;
+    R = (a<b);
+    printf("%d\t",R);
+
+    R = (a>b);
+    printf("%d\t",R);
+
+    R = (a<=b);
+    printf("%d\t",R);
+
+    R = (a>=b);
+    printf("%d\t",R);
+
+    R = (a==b);
+    printf("%d\t",R);
+
+    R = (a!=b);
+    printf("%d\t",R);
+
+}
