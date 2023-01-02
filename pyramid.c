9ee88bf2ed92c807f5af0973db798911ecb2acc6
Manish Kumar Singh <91113108+Manishkumarsingh41@users.noreply.github.com>
2023-01-02 22:14:00 +0530

    Add files via upload
    C program that will print a pyramid shape

---
+++ pyramid.c
@@ -0,0 +1,17 @@
+#include <stdio.h>
+
+int main(void) {
+    int rows = 10;
+    for (int i = 0; i < rows; i++)
+    {
+        for (int j = 0; j < rows - i - 1; j++) {
+            printf(" ");
+        }
+        for (int j = 0; j <= i; j++) {
+            printf("* ");
+        }
+        printf("\n");
+    }
+
+    return 0;
+}
