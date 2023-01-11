ed7f946539a69cf5eb66833067530907632dbbe0
Manish Kumar Singh <91113108+Manishkumarsingh41@users.noreply.github.com>
2023-01-11 22:33:10 +0530

    Add files via upload
    C program for check positive or negative number

---
+++ positive and negative.c
@@ -0,0 +1,11 @@
+#include <stdio.h>
+void main ()
+{
+ int n;
+ printf("Enter the number\n");
+ scanf("%d",&n);
+ if(n>0)
+ printf("Number is positive");
+ if(n<0)
+ printf("Number is negative");
+}
